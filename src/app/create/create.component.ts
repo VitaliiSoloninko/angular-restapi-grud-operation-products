a56ca34650110fce27ae7b48a6c46b52ff71d154
22051988Di <vitalii.soloninko@gmail.com>
2025-01-29 13:25:19 +0100

    0.2	Create

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Product } from '../product.interface';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create.component.html',
-  styleUrl: './create.component.scss'
+  styleUrl: './create.component.scss',
 })
 export class CreateComponent {
+  constructor(private productService: ProductService, private router: Router) {}
 
+  formData: Product = {
+    _id: '',
+    title: '',
+    price: 4,
+  };
+
+  create() {
+    this.productService.create(this.formData).subscribe({
+      next: (val) => {
+        this.router.navigate(['/products']);
+      },
+      error: (er) => {
+        console.log(er);
+      },
+    });
+  }
 }
